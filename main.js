6776553067d976bcf394926e8835f4940f1fc473
Aqib Shabir <aqibfaisalshabir@gmail.com>
2024-09-27 21:48:13 +0100

    added sky

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import * as THREE from 'three'
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
+import { Sky } from 'three/addons/objects/Sky.js'
 import {Timer} from 'three/addons/misc/Timer.js'
 import GUI from "lil-gui";
+
 
 // Debug UI
 // const gui = new GUI();
@@ -129,7 +131,7 @@ house.add(walls)
 
 // Roof
 const roof = new THREE.Mesh(
-  new THREE.ConeGeometry(3.5,1.5,4, 10,10),
+  new THREE.ConeGeometry(3.5,1.5,4, 10),
   new THREE.MeshStandardMaterial({
     map: roofColorTexture,
     aoMap: roofARMTexture,
@@ -139,7 +141,7 @@ const roof = new THREE.Mesh(
   })
 )
 roof.position.y = 2.5 + 0.75
-roof.rotation.y = Math.PI / 4
+roof.rotation.y = Math.PI /4
 house.add(roof)
 
 // Door
@@ -313,6 +315,17 @@ ghost2.shadow.camera.far = 10
 ghost3.shadow.mapSize.width = 256
 ghost3.shadow.mapSize.height = 256
 ghost3.shadow.camera.far = 10
+
+// Sky
+const sky = new Sky()
+sky.scale.set(100,100,100)
+scene.add(sky)
+
+sky.material.uniforms['turbidity'].value = 10
+sky.material.uniforms['rayleigh'].value = 3
+sky.material.uniforms['mieCoefficient'].value = 0.1
+sky.material.uniforms['mieDirectionalG'].value = 0.95
+sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95)
 
 // Animation/orbit controls
 const controls = new OrbitControls(camera, canvas)
